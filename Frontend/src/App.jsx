9d2b627d3e609905ff5d569ff3a8ea0948cab809
Nitesh Kadam <niteshkadam1506@gmail.com>
2026-08-22 14:54:52 +0530

    Improve interview progress and routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
@@ -11,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/interview" element={<InterviewSetup />} />
